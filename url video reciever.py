8615c3787f834f028c210514090a51068f476aad
S.K.Roshan <63838060+dev-Roshan-lab@users.noreply.github.com>
2020-07-26 18:11:33 +0530

    Thats all....

---
+++ url video reciever.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy
+
+url = 'http://192.168.43.1:8080/video'
+cap = cv2.VideoCapture(url)
+face = cv2.CascadeClassifier('cascade.xml')
+while(True):
+    ret, frame = cap.read()
+    if frame is not None:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=50,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+        )
+        for (x, y, w, h) in faces :
+            cv2.rectangle(frame,(x,y),(x+w, x+h),(255,0,0),2)
+            cv2.putText(frame, 'Face', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+            print(x)
+        cv2.imshow('frame',frame)
+        
+    q = cv2.waitKey(1)
+    if q == ord("q"):
+        break
+cv2.destroyAllWindows()
